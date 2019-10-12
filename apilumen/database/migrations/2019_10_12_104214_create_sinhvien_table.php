b132ffc15f86313879f6b0268937a0f33533f66f
Nguyễn Xuân Hạnh <48309752+xuanhanhnguyen@users.noreply.github.com>
2019-10-12 17:57:48 +0700

    tạo migration - model

---
+++ apilumen/database/migrations/2019_10_12_104214_create_sinhvien_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSinhvienTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sinhvien', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sinhvien');
+    }
+}
