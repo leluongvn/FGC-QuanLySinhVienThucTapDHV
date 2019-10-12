e96f4dff4b8d60a56b41aa942daab6c7038ab29c
Nguyễn Xuân Hạnh <48309752+xuanhanhnguyen@users.noreply.github.com>
2019-10-12 17:57:04 +0700

    phân công việc viết routes

--- apilumen/routes/web.php
+++ apilumen/routes/web.php
@@ -14,3 +14,37 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+/*
+ - code api
+ - yêu cầu các bạn sau mỗi dòng code phải có chú thích vào
+
+-VD:
+ //tạo để tài mới
+ $router->get('detai',['uses'=>'detaiController@create']);
+*/
+$router->group(['prefex'=>'api'],function() use ($router){
+
+    // code phần giáo viên
+    $router->group(['prefix'=>'giaovien'],function() use ($router){
+        // mại code
+
+    });
+    // code phần sinh viên
+    $router->group(['prefix'=>'sinhvien'],function() use ($router){
+        // huy code
+
+    });
+
+    //code phần doanh nghiệp
+    $router->group(['prefix'=>'doanhnghiep'],function() use ($router){
+        //Đạt code
+
+    });
+
+    // code phần đề tài
+    $router->group(['prefix'=>'detai'],function() use ($router){
+        // Bá code
+        
+    });
+});
