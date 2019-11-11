5977a4e0c694b76e4675f466ce901a20b5cff7ce
Nguyễn Văn Thành <48385193+congnghecao@users.noreply.github.com>
2019-11-11 16:32:24 +0700

    Create QLGiaovien - Clinet
    - Datatable
    - Add/Edit/Delete Data
    - Modal Add/Edit .
    - Vue Noty

--- server/app/Model/Teacher.php
+++ server/app/Model/Teacher.php
@@ -9,6 +9,6 @@ class Teacher extends Model
     public $timestamps = false;
 
     protected $table='teachers';
-    protected $fillable = ['magv','id_subject', 'name','password', 'email','phone','introduce'];
+    protected $fillable = ['msgv','id_subject', 'name','password', 'email','phone','introduce'];
 
 }
